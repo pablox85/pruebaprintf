30236c7071cce30bedba67f880fd89a316b3e63e
pablox85 <pdpcorrales@gmail.com>
2025-03-27 23:33:43 +0000

    a

---
+++ casos.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdarg.h>
+
+int especifico (va_lis args, char especifico)
+{
+	int count = 0;
+	
+	switch (especifico)
+	{
+		case 'c':
+			count += print_char(args);
+			break;
+		case 's':
+			count += print_string(args);
+			break;
+		case '%':
+			count += print_porciento(args);
+			break;
+		case 'd':
+		case 'i':
+			count += print_int(args);
+			break;
+		default:
+			count += write(1, "%", 1);
+			count += write(1, &especifico, 1);
+	}
+	return (count);
+}
+
